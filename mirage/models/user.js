5a692487274a1f1f545784775e8efc239cfb904c
Nikola Begedin <begedinnikola@gmail.com>
2016-09-23 19:17:16 +0200

    Fix failing tests by relying on coalesce requests and minimizing many-to-many relationships

--- mirage/models/user.js
+++ mirage/models/user.js
@@ -1,7 +1,6 @@
 import { Model, hasMany } from 'ember-cli-mirage';
 
 export default Model.extend({
-  organizations: hasMany({ inverse: 'members' }),
   organizationMemberships: hasMany({ inverse: 'member' }),
 
   categories: hasMany(),
