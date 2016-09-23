f42a1e99e4cfdda65a36e7acfb31ec4ea300d4ce
Nikola Begedin <begedinnikola@gmail.com>
2016-09-23 19:36:28 +0200

    Cleanup of remainder from mirage

--- mirage/models/user.js
+++ mirage/models/user.js
@@ -2,7 +2,6 @@ import { Model, hasMany } from 'ember-cli-mirage';
 
 export default Model.extend({
   organizationMemberships: hasMany({ inverse: 'member' }),
-
   categories: hasMany(),
   userCategories: hasMany(),
   userRoles: hasMany(),
